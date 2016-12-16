54b9f76523b33c5f7c2936c00d9b8b4ddb56a82c
Kaveh Shamsi <mshamsi@pivotal.io>
2016-12-16 13:35:29 +0100

    keep the broker password out of repo
    Signed-off-by: Max Brauer <mbrauer@pivotal.io>

--- broker/deploy.sh
+++ broker/deploy.sh
@@ -10,14 +10,24 @@ serviceHost() {
     echo `cf app simple-hive-service | grep urls | cut -d " " -f 2`
 }
 
+assertBrokerAdminPasswordSet() {
+    if [ "$BROKER_ADMIN_PASSWORD" = "" ]; then
+        echo 'Error: Make sure BROKER_ADMIN_PASSWORD is set.'
+        exit 1
+    fi
+}
+
 hydrateEnv() {
     cf set-env simple-hive-broker SIMPLE_HIVE_SERVICE_HOST $(serviceHost)
     cf set-env simple-hive-broker SIMPLE_HIVE_SERVICE_PORT 80
+    cf set-env simple-hive-broker SECURITY_USER_PASSWORD ${BROKER_ADMIN_PASSWORD}
 }
 
 cd $(dirname $0)
 
 ../gradlew :broker:assemble
+
+assertBrokerAdminPasswordSet
 
 hydrateEnv
 
@@ -27,4 +37,4 @@ cf purge-service-offering simple-hive -f
 
 cf delete-service-broker simple-hive-broker -f
 
-cf create-service-broker simple-hive-broker user honey http://$(brokerHost) --space-scoped
+cf create-service-broker simple-hive-broker user ${BROKER_ADMIN_PASSWORD} http://$(brokerHost) --space-scoped
